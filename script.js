fac44c1a66dad2e8198fb984a9cd2657feb25efa
pdv88 <888pdv@gmail.com>
2022-11-18 22:47:06 -0600

    add startGame function that makes grid and options

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// ------ Making start game button ------
+
 const board = document.getElementById('board')
 const startGameBtn = document.getElementById('start_game_btn')
 startGameBtn.addEventListener('click', startGame)
 
 let cells = []
+
+// ------ function to set up game board ------
 
 function startGame () {
     startGameBtn.remove()
@@ -10,8 +14,34 @@ function startGame () {
     grid.classList.add('grid')
     board.appendChild(grid)
     
-    const cell = document.createElement('div')
+    // ------ loop to make grid ------
     
-    
+    for(let i = 1; i <= 9; i++) {
+        const cell = document.createElement('div')
+        //cell.addEventListener('click', makeMove)
+        cell.classList.add('cell')
+        cell.setAttribute('id', [i])
+        grid.appendChild(cell)
+    }
+
+    // ------ create options div and buttons ------
+
+    const options = document.createElement('div')
+    options.classList.add('options')
+    board.appendChild(options)
+
+    // ------ create player vs player button ------
+
+    const pvpBtn = document.createElement('button')
+    pvpBtn.classList.add('pvpBtn')
+    pvpBtn.textContent = "Player vs Player"
+    options.appendChild(pvpBtn)
+
+    // ------ create playes vs AI button ------
+
+    const pvcBtn = document.createElement('button')
+    pvcBtn.classList.add('pvcBtn')
+    pvcBtn.textContent = "Player vs AI"
+    options.appendChild(pvcBtn)
 
 }
